3f03ba46a12e2be0007237a5abeb69b33a849266
kuchinsk1y <98710097+kuchinsk1y@users.noreply.github.com>
2025-06-29 15:57:41 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { createAdvertisements } from './data.js'
-import { createOfferCard } from './card.js'
+// import { createOfferCard } from './card.js'
 import './form.js'
+import './map.js'
 
 
 const SIMILAR_ADS_COUNT = 10
@@ -9,8 +10,8 @@ const similarAds = createAdvertisements(SIMILAR_ADS_COUNT)
 // eslint-disable-next-line no-console
 console.log(similarAds)
 
-const mapCanvas = document.querySelector('#map-canvas')
+// const mapCanvas = document.querySelector('#map-canvas')
 
-const firstAd = similarAds[0]
-const offerCard = createOfferCard(firstAd)
-mapCanvas.appendChild(offerCard)
+// const firstAd = similarAds[0]
+// const offerCard = createOfferCard(firstAd)
+// mapCanvas.appendChild(offerCard)
